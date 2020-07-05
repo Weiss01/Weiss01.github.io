68889967d89e5105d897d7684dac8256c6db2244
Fabian Lim <fabianlimxuhuai@gmail.com>
2020-07-06 02:50:36 +0800

    ok

--- js/myscript.js
+++ js/myscript.js
@@ -41,45 +41,37 @@ function add_alert() {
     $('.jumbotron').append(alert);
 }
 
-<div class="input-group">
-  <div class="input-group-prepend">
-    <span class="input-group-text" id="">First and last name</span>
-  </div>
-  <input type="text" class="form-control">
-  <input type="text" class="form-control">
-</div>
 
 function addParseOptions() {
     var input_div = document.createElement("div");
-    input_div.setAttribute('class', 'input-group mb-3');
+    input_div.setAttribute('class', 'input-group');
     input_div.setAttribute('id', 'input_div');
-    var inner_input_div = document.createElement("div");
-    inner_input_div.setAttribute('class', 'input-group-prepend');
-    inner_input_div.setAttribute('id', 'inner_input_div');
-    var input_span = document.createElement("span");
-    input_span.setAttribute('class', 'input-group-text');
-    input_span.setAttribute('id', 'inputGroup-sizing-default');
-    var input_box = document.createElement("input");
-    input_box.setAttribute('type', 'text');
-    input_box.setAttribute('class', 'form-control');
-    input_box.setAttribute('aria-label', 'Default');
-    input_box.setAttribute('aria-describedby', 'inputGroup-sizing-default');
-    var parse_option_div = document.createElement("div");
-    parse_option_div.setAttribute('class','container');
-    parse_option_div.setAttribute('id','parse_option');
+    var input_box1 = document.createElement("input");
+    input_box1.setAttribute('type', 'text');
+    input_box1.setAttribute('class', 'form-control');
+    input_box1.setAttribute('id', 'input_box1');
+    input_box1.setAttribute('placeholder', 'Lines to Remove');
+    var input_box2 = document.createElement("input");
+    input_box2.setAttribute('type', 'text');
+    input_box2.setAttribute('class', 'form-control');
+    input_box2.setAttribute('id', 'input_box2');
+    input_box1.setAttribute('placeholder', 'Last k Reproducibility Run');
+    var input_box3 = document.createElement("input");
+    input_box3.setAttribute('type', 'text');
+    input_box3.setAttribute('class', 'form-control');
+    input_box3.setAttribute('id', 'input_box3');
     var button = document.createElement("button");
     var breakpoint = document.createElement("br");
     button.setAttribute('id', 'parse');
     button.setAttribute('class', 'btn btn-outline-secondary');
     button.setAttribute('type', 'button');
-    button.setAttribute('style', 'float: right');
     $('.jumbotron').append(breakpoint);
-    $('.jumbotron').append(parse_option_div);
-    $('#parse_option').append(input_div);
-    $('#input_div').append(inner_input_div);
-    $('#inner_input_div').append(input_span);
-    $('#input_div').append(input_box);
-    $('#parse_option').append(button);
+    $('.jumbotron').append(input_div);
+    $('#input_div').append(input_box1);
+    $('#input_div').append(input_box2);
+    $('#input_div').append(input_box3);
+    $('.jumbotron').append(breakpoint);
+    $('.jumbotron').append(button);
     $('#parse').html("Parse");
     process_button = $("#parse");
 
@@ -114,6 +106,10 @@ function alert_complete() {
     $('#alert-bar').text("Finished Parsing " + file_list[0].name);
 }
 
+function getInputs() {
+    input = [];
+
+}
 
 
 function start_process(papaparse_object) { // papaparse_object -> {data: Array(4), errors: Array(1), meta: {…}}
