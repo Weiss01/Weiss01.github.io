850b41198c6037d5dd4dde7341d2a0dd40617122
Fabian Lim <fabianlimxuhuai@gmail.com>
2020-07-05 14:08:24 +0800

    completed csv parsing feature

--- js/myscript.js
+++ js/myscript.js
@@ -1,3 +1,16 @@
+const fileSelector = $('#inputFile');
+const popup = $('.alert');
+var file_list;
+var file_content;
+var config = getConfig();
+var results;
+
+function getConfig() {
+    return {
+    delimiter: ",",
+  }
+}
+
 function readFileAsString(files) {
     if (files.length === 0) {
         console.log('No file is selected');
@@ -5,14 +18,28 @@ function readFileAsString(files) {
     }
     var reader = new FileReader();
     reader.onload = function(event) {
-        console.log('File content:', event.target.result);
         file_content = event.target.result;
+        results = Papa.parse(file_content);
     };
     reader.readAsText(files[0]);
 }
 
+fileSelector.change(function (event) {
+  popup.addClass('unhide');
+  const fileList = event.target.files;
+  file_list = fileList;
+  $(".custom-file-label").text(file_list[0].name);
+  popup.text("File Name: " + file_list[0].name + ", File Type: " + file_list[0].type + ", File Size: " + file_list[0].size  + "kB");
+  if ($('button').length < 2){
+    addButton();
+  }
+})
+
+function start_process() {
+  console.log(results);
+}
+
 function addButton() {
-  // <button class="btn btn-outline-secondary" type="button" id="inputFileAddon">Upload</button>
   var button = document.createElement("button");
   var breakpoint = document.createElement("br");
   button.setAttribute('id', 'process');
@@ -25,27 +52,7 @@ function addButton() {
 
   process_button.click(function () {
     console.log("Starting Processing");
-    console.log(myfile);
-    readFileAsString(myfile);
-    calculate();
+    readFileAsString(file_list);
+    start_process();
   })
-}
-
-const fileSelector = $('#inputFile');
-const popup = $('.alert');
-var myfile;
-var file_content;
-
-fileSelector.change(function (event) {
-  popup.addClass('unhide');
-  const fileList = event.target.files;
-  console.log(fileList);
-  myfile = fileList;
-  $(".custom-file-label").text(fileList[0].name);
-  popup.text("File Name: " + fileList[0].name + ", File Type: " + fileList[0].type + ", File Size: " + fileList[0].size  + "kB");
-  addButton();
-})
-
-function calculate() {
-  console.log(Papa.parse(file_content));
 }
